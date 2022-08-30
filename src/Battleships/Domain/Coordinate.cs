d366c3b096f3c05c2e9b67a1314376a24e9d81d1
t-wyrwas <tomasz.wyrwas@gmail.com>
2022-08-30 09:22:31 +0200

    Refactor: remove redundant checks in Coordinate

--- src/Battleships/Domain/Coordinate.cs
+++ src/Battleships/Domain/Coordinate.cs
@@ -2,14 +2,14 @@ namespace Battleships.Domain;
 
 public record struct Coordinate
 {
-    public const int ASCII_CODE_A = 65;
     public int X { get; }
     public int Y { get; }
+
     public Coordinate(int x, int y)
     {
         var invalidX = x < 1 || x > 10;
         var invalidY = y < 1 || y > 10;
-        if(invalidX || invalidY)
+        if (invalidX || invalidY)
         {
             throw new ArgumentException($"Invalid coordinates: {x}:{y}");
         }
@@ -24,20 +24,19 @@ public record struct Coordinate
         {
             throw new ArgumentException($"Invalid cooridnate string: {value}");
         }
+
         value = value.ToUpper();
+
         var xCoordinateStr = value.Substring(0, 1);
-        var xCoordinateValid = "ABCDEFGHIJ".Contains(xCoordinateStr); // TODO
         var xCoordinate = ((int)xCoordinateStr.ToCharArray()[0] + 1 - ASCII_CODE_A);
+
         var yCoordinateStr = value.Substring(1, value.Length - 1);
         int yCoordinate = default(int);
         int.TryParse(yCoordinateStr, out yCoordinate);
-        var yCoordinateValid = yCoordinate >= 1 && yCoordinate <= 10;
-
-        if (!xCoordinateValid || !yCoordinateValid)
-        {
-            throw new ArgumentException($"Invalid cooridnate string: {value}");
-        }
 
         return new Coordinate(xCoordinate, yCoordinate);
     }
+
+    private const int ASCII_CODE_A = 65;
+    private const string X_AXIS_VALUES = "ABCDEFGHIJ";
 }
